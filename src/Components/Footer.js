c7dad4d8adbadf5e199cfc8717b2196ff596fc87
lmariel <laurenmariel90@gmail.com>
2017-08-01 10:27:04 -0400

    Add styled-components to Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import styled from 'styled-components';
 import { Grid, Row, Col } from 'react-bootstrap';
 
-const Footer = props => {
-  return (
-    <div className="page-footer">
-      <Grid>
-        <Row>
-          <Col xs={12}>
-            <p>
-              {props.copyright}
-            </p>
-          </Col>
-        </Row>
-      </Grid>
-    </div>
-  );
-};
+const Footer = styled.footer`
+  padding: 20px;
+  color: #f00;
+`;
 
-Footer.propTypes = {
-  copyright: PropTypes.string.isRequired,
-};
-
-export default Footer;
+export default class Container extends React.Component {
+  static propTypes = {
+    copyright: PropTypes.string.isRequired,
+  };
+  render() {
+    const { copyright } = this.props;
+    return (
+      <div className="page-footer">
+        <Footer>
+          <Grid>
+            <Row>
+              <Col xs={12}>
+                <p>
+                  {copyright}
+                </p>
+              </Col>
+            </Row>
+          </Grid>
+        </Footer>
+      </div>
+    );
+  }
+}
