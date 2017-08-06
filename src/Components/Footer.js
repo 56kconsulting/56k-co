0779d73f666662ac7082afd4ebff468eed0205e8
lmariel <laurenmariel90@gmail.com>
2017-08-06 11:18:16 -0400

    Add social icons and links to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import FaTwitter from 'react-icons/lib/fa/twitter';
+import FaMedium from 'react-icons/lib/fa/medium';
+import FaLinkedin from 'react-icons/lib/fa/linkedin';
+import FaGithub from 'react-icons/lib/fa/github';
+import FaEnvelope from 'react-icons/lib/fa/envelope';
+
 import { Grid, Row, Col } from 'react-bootstrap';
 
 const Footer = styled.footer`padding: 20px;`;
@@ -11,17 +17,36 @@ export default class Container extends React.Component {
         <Footer>
           <Grid>
             <Row>
-              <Col xs={12} md={6}>
+              <Col xs={2} xsOffset={1}>
+                <a href="https://twitter.com/Abrongersma" target="_blank">
+                  <FaTwitter />
+                </a>
+              </Col>
+              <Col xs={2}>
+                <a href="https://medium.com/@abrongersma" target="_blank">
+                  <FaMedium />
+                </a>
+              </Col>
+              <Col xs={2}>
+                <a
+                  href="https://www.linkedin.com/in/abrongersma/"
+                  target="_blank"
+                >
+                  <FaLinkedin />
+                </a>
+              </Col>
+              <Col xs={2}>
+                <a href="https://twitter.com/Abrongersma" target="_blank">
+                  <FaGithub />
+                </a>
+              </Col>
+              <Col xs={2}>
+                <FaEnvelope />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12}>
                 <p>&copy; 2017 56 K Consulting, LLC</p>
-              </Col>
-              <Col xs={4} md={2}>
-                Twitter
-              </Col>
-              <Col xs={4} md={2}>
-                Medium
-              </Col>
-              <Col xs={4} md={2}>
-                LinkedIn
               </Col>
             </Row>
           </Grid>
